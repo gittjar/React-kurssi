5133185369f6026052e7ee84b650f075a1ab079b
gittjar <jarno.keranen@gmail.com>
2023-09-13 21:37:58 +0300

    eka step valmis

--- osa1/src/App.jsx
+++ osa1/src/App.jsx
@@ -1,3 +1,13 @@
+const Content = () => {
+  return (
+    <div>
+      <Part1 /> ja tehtävämäärä: <Exercises1 />
+      <Part2 /> ja tehtävämäärä: <Exercises2 />
+      <Part3 /> ja tehtävämäärä: <Exercises3 />
+    </div>
+  )
+}
+
 
 
 const Course = () => {
@@ -13,8 +23,9 @@ const Part1 = () => {
 }
 
 const Exercises1 = () => {
+  const a = 10
   return (
-    10
+   a
   )
 }
 const Part2 = () => {
@@ -23,8 +34,9 @@ const Part2 = () => {
   )
 }
 const Exercises2 = () => {
+  const b = 7
   return (
-    7
+    b
   )
 }
 const Part3 = () => {
@@ -33,28 +45,49 @@ const Part3 = () => {
   )
 }
 const Exercises3 = () => {
+  const c = 14
   return (
-    14
+    c
   )
 }
 
+const TotalSum = () => {
+  const aValue = Exercises1();
+  const bValue = Exercises2();
+  const cValue = Exercises3();
+  const sum = aValue + bValue + cValue;
+
+  return (
+    <div>
+      <p>Kokonaismäärä tehtäviä: {sum}</p>
+    </div>
+  );
+};
 
 const App = () => {
   
   return (
     
     <div>
+      <h1>Step 1</h1>
      <h1><Course /></h1>
-      
-      <Part1 />
-       <Exercises1 /> 
-   
-       <Part2 />
-       <Exercises2 />
+        <Part1 />
+        <Exercises1 /> 
+        <Part2 />
+        <Exercises2 />
+        <Part3 /> 
+        <Exercises3 />
 
-      
-        <Part3 /> <Exercises3 />
+      <h1>Step 2</h1> 
+      <Content />
+
+
+      <h2>
+      <TotalSum />
+      </h2>
         </div>
+
+    
   
   
   )
