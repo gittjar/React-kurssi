b1550219cc601c6f7b0c1f9ab19ba1bf4075549d
gittjar <jarno.keranen@gmail.com>
2023-09-14 19:19:09 +0300

    counter ticking after 1s continous

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,14 +1,18 @@
-const App = (props) => {
-  const {counter} = props
-  return (
+import { useState } from 'react'
 
-    <div>
-      <h1>Laskuri</h1>
-      <h2>{counter}</h2>
-      
-      </div>
+const App = () => {
+
+  const [ counter, setCounter ] = useState(0)
+
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  return (
+    <h3>{counter}</h3>
   )
 }
-
 
 export default App
