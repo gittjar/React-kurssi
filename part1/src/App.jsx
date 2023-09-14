12f958fea7ba27450df055f1ba7482b2792f4fcf
gittjar <jarno.keranen@gmail.com>
2023-09-14 19:15:28 +0300

    counter

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,49 +1,14 @@
-const Hello = (props) => {
-
-  props = {
-    name: 'Pekto',
-    age: 36,
-  }
-  const name = props.name
-  const age = props.age
-
-
-  const bornYear = () => new Date().getFullYear() - age
-
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
 
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born {bornYear()}</p>
-    </div>
+    <div>
+      <h1>Laskuri</h1>
+      <h2>{counter}</h2>
+      
+      </div>
   )
 }
 
-// footer
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by :
-      <a href="https://github.com/gittjar">gittjar</a>
-    </div>
-  )
-}
-
-const App = () => {
-
-  const nimi = 'Pekka'
-  const ika = 10
-
-
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello />
-      <Footer/>
-    </div>
-    
-  )
-}
 
 export default App
