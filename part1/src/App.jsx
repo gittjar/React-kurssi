694486a872b0937d3c430746078ed38acc909e0b
gittjar <jarno.keranen@gmail.com>
2023-09-16 10:18:26 +0300

    counter and minus-zero-plus ready

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -3,6 +3,27 @@ import { useState } from 'react'
 const App = () => {
 
   const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
+
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+  
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
+  const handleClick = () => {
+    console.log('clicked')
+  }
 
 
   setTimeout(
@@ -10,8 +31,23 @@ const App = () => {
     1000
   )
 
+  const Display = ({ counter }) => <div>{counter}</div>
+
+  const Button = (props) => {
+    return (
+      <button onClick={props.handleClick}>
+        {props.text}
+      </button>
+    )
+  }
+
   return (
-    <h3>{counter}</h3>
+    <div>
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text="plus" />
+      <Button handleClick={setToZero} text="zero" />
+      <Button handleClick={decreaseByOne} text="minus" />
+    </div>
   )
 }
 
