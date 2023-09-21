479d66da9e9deffb037a0f1b5b6906db77971750
gittjar <jarno.keranen@gmail.com>
2023-09-21 08:48:27 +0300

    2.9 puhelinluettelo step 4 (filter names and numbers)

--- osa2b/src/App.jsx
+++ osa2b/src/App.jsx
@@ -1,24 +1,34 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Päiviö Pyysalo', puhelin: '0500123123' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', puhelin: '040-123456' },
+    { name: 'Ada Lovelace', puhelin: '39-44-5323523' },
+    { name: 'Dan Abramov', puhelin: '12-43-234345' },
+    { name: 'Mary Poppendieck', puhelin: '39-23-6423122' },
+    { name: 'Terminator "T-1000"', puhelin: '555-441000123' },
+    { name: 'Jack Sparrow', puhelin: '041-9996660' },
+  ]);
+
   const [newName, setNewName] = useState('');
   const [newPhoneNumber, setNewPhoneNumber] = useState('');
-
   const [errorMessage, setErrorMessage] = useState('');
-
+  const [filterText, setFilterText] = useState('');
+  const [filteredList, setFilteredList] = useState(persons);
 
   const addName = (event) => {
     event.preventDefault();
 
     if (persons.some((person) => person.name === newName)) {
-      setErrorMessage(`'${newName}' on jo listalla!`);
+      setErrorMessage(`'${newName}' is already in the list.`);
     } else {
       const newPerson = { name: newName, puhelin: newPhoneNumber };
       setPersons([...persons, newPerson]);
       setNewName('');
       setNewPhoneNumber('');
       setErrorMessage('');
+      // Update filtered list with the new data
+      setFilteredList([...filteredList, newPerson]);
     }
   };
 
@@ -30,43 +40,48 @@ const App = () => {
     setNewPhoneNumber(event.target.value);
   };
 
-
-  const checkTextInput = () => {
-    //Check for the Name TextInput
-    if (!newName.trim()) {
-      alert('Please Enter Name');
-      return;
-    }}
-
-
-
-
-
+  const handleFilterChange = (event) => {
+    const searchText = event.target.value;
+    setFilterText(searchText);
+    // Update filtered list based on the search text
+    const filtered = persons.filter((person) =>
+      person.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilteredList(filtered);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter phonebook: <br />
+        <input value={filterText} onChange={handleFilterChange} />
+      </div>
       <form onSubmit={addName}>
         <div>
-          name: <br />
+          Name: <br />
           <input value={newName} onChange={handleNameChange} />
         </div>
-        <div>number: <br/>
-          <input value={newPhoneNumber} onChange={handlePhoneNumberChange} /></div>
-
         <div>
-          <button type="submit" onClick={checkTextInput}>add</button>
+          Number: <br />
+          <input value={newPhoneNumber} onChange={handlePhoneNumberChange} />
+        </div>
+        <div>
+          <button type="submit">Add</button>
         </div>
       </form>
       {errorMessage && <div className="error">{errorMessage}</div>}
-
-      <div>debug: {newName}</div>
       <h2>Numbers</h2>
       <table>
-        {persons.map((person, index) => (
-          <tr key={index}>{person.name} {person.puhelin}</tr>
-        ))}
-    </table>
+        <tbody>
+          {filteredList.map((person, index) => (
+            <tr key={index}>
+              <td>{person.name}</td>
+              <td>{person.puhelin}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
