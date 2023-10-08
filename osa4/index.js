2d2daa5c9b831c9a63d4fb48e8a6460cac9782fc
gittjar <jarno.keranen@gmail.com>
2023-10-08 19:33:15 +0300

    4.2 blogilista step 2 (ohjelma jaettu useampaan moduuliin)

--- osa4/index.js
+++ osa4/index.js
@@ -3,65 +3,54 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-const Bloglist = require('./models/bloglist'); // Import the Bloglist model
+const logger = require('./utils/logger');
+const blogsRouter = require('./routes/blogs');
+const Bloglist = require('./models/bloglist');
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect to your MongoDB database using the provided URI in .env
+// Connect to MongoDB
 const url = process.env.MONGODB_URI;
-console.log('Im message from index.js');
-
+logger.info('Connecting to MongoDB...');
 mongoose
   .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('Connected to MongoDB');
+    logger.info('Connected to MongoDB success!');
   })
   .catch((error) => {
-    console.error('Error connecting to MongoDB:', error.message);
+    logger.error('Error connecting to MongoDB:', error.message);
   });
-  
 
-  app.get('/api/blogs', async (request, response) => {
-    try {
-      const blogs = await Bloglist.find({});
-      response.json(blogs);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      response.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-  
-  app.post('/api/blogs', async (request, response) => {
-    const blog = new Bloglist(request.body);
-  
-    try {
-      const savedBlog = await blog.save();
-      response.status(201).json(savedBlog);
-    } catch (error) {
-      console.error('Error saving data:', error);
-      response.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-  
+// Routes
+app.use('/api/blogs', blogsRouter);
 
-  app.get('/info', async (req, res) => {
-    try {
-      const blogCount = await Bloglist.countDocuments({});
-      const currentTime = new Date().toString();
-      res.send(`
-        <div style="background-color: navy; height: 100vh; display: flex; flex-direction: column; justify-content: center; align-items: center;">
-          <h2 style="color: fuchsia;">Bloglist has info for ${blogCount} blogs</h2>
-          <span style="font-size: 1.4rem; color: green; background-color: black; padding: 1rem; border-radius: 0.8rem;">Today is: ${currentTime}</span>
-        </div>
-      `);
-    } catch (error) {
-      console.error('Error counting blogs:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+const startTime = new Date();
+
+app.get('/info', async (req, res) => {
+  try {
+    const blogCount = await Bloglist.countDocuments({});
+    const currentTime = new Date().toString();
+    const uptimeInSeconds = Math.floor((new Date() - startTime) / 1000);
+
+    res.send(`
+      <div style="background-color: navy; height: 100vh; display: flex; flex-direction: column; justify-content: center; align-items: center;">
+        <h2 style="color: fuchsia;">Bloglist has info for ${blogCount} blogs</h2>
+        <h2 style="color: white;">Read blogs: /api/blogs</h2>
+        <h2 style="color: fuchsia;">Server Status: Online</h2>
+        <h2 style="color: white;">Server Uptime: ${uptimeInSeconds} seconds</h2>
+        <span style="font-size: 1.4rem; color: green; background-color: black; padding: 1rem; border-radius: 0.8rem;">Current Time: ${currentTime}</span>
+      </div>
+    `);
+  } catch (error) {
+    logger.error('Error counting blogs:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 
 const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  logger.info(`Server running on port ${PORT}`);
 });
--- osa4/models/bloglist.js
+++ osa4/models/bloglist.js
@@ -1,31 +1,18 @@
-const mongoose = require('mongoose')
-
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to ...')
-mongoose.connect(url)
-  .then(result => {
-    console.log('Connected to MongoDB !')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+const mongoose = require('mongoose');
 
 const bloglistSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number,
-})
+});
 
 bloglistSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Bloglist', bloglistSchema)
+module.exports = mongoose.model('Bloglist', bloglistSchema);
