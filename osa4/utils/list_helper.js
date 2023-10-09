040fadb317b2fb38381a040550a830b39b816f8f
gittjar <jarno.keranen@gmail.com>
2023-10-09 18:54:36 +0300

    4.4 apufunktioita ja yksikkötestejä, step 2 (equals total likes)

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,9 +1,14 @@
-const dummy = (blogs) => {
+const dummy = () => {
     return 1; // Return a fixed value of 1, as this is what the test expects
   }
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((total, blog) => total + blog.likes, 0);
+}  
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
   
   
