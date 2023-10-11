9a2febbcad290720aad6a0f2050d3f06326c4d42
gittjar <jarno.keranen@gmail.com>
2023-10-11 18:38:52 +0300

    4.10 blogilistan testit, step 3 (HTTP POST valid blog)

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -13,7 +13,7 @@ describe('Blog API tests', () => {
       .expect(200)
       .expect('Content-Type', /application\/json/);
   });
-
+// blog contains id property
   test('blogs have an "id" property', async () => {
     const response = await api.get('/api/blogs');
     const blogs = response.body;
@@ -22,6 +22,41 @@ describe('Blog API tests', () => {
       expect(blog.id).toBeDefined();
     });
   });
+
+// test valid blog
+test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'Test Blog',
+      author: 'Test Author',
+      url: 'https://testblog.com',
+      likes: 5,
+    };
+  
+    // Get the initial number of blogs
+    const initialResponse = await api.get('/api/blogs');
+    const initialBlogs = initialResponse.body;
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+  
+    // Check if the total number of blogs has increased by 1
+    const response = await api.get('/api/blogs');
+    expect(response.body.length).toBe(initialBlogs.length + 1);
+  
+    // Check if the new blog contains the expected properties
+    const addedBlog = response.body.find((blog) => blog.title === 'Test Blog');
+    expect(addedBlog).toBeDefined();
+    expect(addedBlog.title).toBe('Test Blog');
+    expect(addedBlog.author).toBe('Test Author');
+    expect(addedBlog.url).toBe('https://testblog.com');
+    expect(addedBlog.likes).toBe(5);
+  });
+  
+  
+  
 
 
 });
