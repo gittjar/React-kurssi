e441357d42e59c51d579359f55645c6584513658
gittjar <jarno.keranen@gmail.com>
2023-10-29 15:19:41 +0200

    update

--- osa4/middleware/middleware.js
+++ osa4/middleware/middleware.js
@@ -1,8 +1,7 @@
-// middleware.js
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (request, response, next) => {
-  const token = request.get('authorization'); // Ota token otsakkeesta
+  const token = request.get('authorization'); // Get token from the header
 
   if (!token) {
     return response.status(401).json({ error: 'Token missing' });
@@ -10,7 +9,7 @@ const verifyToken = (request, response, next) => {
 
   try {
     const decodedToken = jwt.verify(token, process.env.SECRET);
-    request.user = decodedToken; // Lisää dekoodattu token request-objektiin
+    request.user = decodedToken; // Add the decoded token to the request object
     next();
   } catch (error) {
     return response.status(401).json({ error: 'Token invalid' });
