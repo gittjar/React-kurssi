7c15845442268053b56a83d332498d952eb5739a
gittjar <jarno.keranen@gmail.com>
2023-12-16 15:50:36 +0200

    part 8c created

--- osa8/library-frontend/src/components/EditAuthor.js
+++ osa8/library-frontend/src/components/EditAuthor.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { useMutation, gql } from '@apollo/client';
+import { useMutation, useQuery, gql } from '@apollo/client';
+import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
 
 const SET_AUTHOR_BORN = gql`
   mutation setAuthorBorn($name: String!, $born: Int!) {
@@ -10,7 +11,16 @@ const SET_AUTHOR_BORN = gql`
   }
 `;
 
+const GET_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+    }
+  }
+`;
+
 const AuthorEdit = () => {
+  const { loading, error: queryError, data } = useQuery(GET_AUTHORS);
   const [authorName, setAuthorName] = useState('');
   const [bornYear, setBornYear] = useState('');
   const [error, setError] = useState(null);
@@ -30,25 +40,42 @@ const AuthorEdit = () => {
     } catch (error) {
       console.error('Error updating author born year:', error);
       setError('Sorry, no name found!');
+      setAuthorName('');
+      setBornYear('');
     }
   };
 
+  if (loading) return <p>Loading...</p>;
+  if (queryError) return <p>Error fetching authors: {queryError.message}</p>;
+
+  const authors = data.allAuthors;
+
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Author Name"
+      <h2>edit author</h2>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      author name<br></br>
+      <select
         value={authorName}
         onChange={(event) => setAuthorName(event.target.value)}
-      />
+      >
+        <option value="" disabled>Select an author</option>
+        {authors.map((author) => (
+          <option key={author.name} value={author.name}>
+            {author.name}
+          </option>
+        ))}
+      </select><br></br>
+      born year<br></br>
       <input
         type="number"
         placeholder="Born Year"
         value={bornYear}
         onChange={(event) => setBornYear(event.target.value)}
-      />
-      <button onClick={updateAuthorBorn}>Update Author</button>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+      /><br></br>
+      <button onClick={updateAuthorBorn}>update author
+      <ArrowForwardIosRoundedIcon className='iconfwd' fontSize="small"></ArrowForwardIosRoundedIcon>
+      </button>
     </div>
   );
 };
