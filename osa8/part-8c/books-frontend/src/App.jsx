6d13a0517c3066fadd12bfc24b7c6167aad03212
gittjar <jarno.keranen@gmail.com>
2023-12-25 17:36:45 +0200

    8.18 kirjautuminen (login works)

--- osa8/part-8c/books-frontend/src/App.jsx
+++ osa8/part-8c/books-frontend/src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 import './styles.css';
 import BooksList from './components/BooksList';
+import LoginForm from './components/LoginForm';
+import { useState } from 'react';
+
+
 // Initialize Apollo Client
 const client = new ApolloClient({
   uri: 'http://localhost:4000', // replace with your server's URL
@@ -14,10 +18,44 @@ const client = new ApolloClient({
 
 // Main App component
 function App() {
+
+  const [token, setToken] = useState(localStorage.getItem('user-token'));
+  const [username, setUsername] = useState(null); // new state variable for the username
+
+  const handleLogin = (newToken, username) => {
+    setToken(newToken);
+    setUsername(username); // set the username
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('user-token');
+    setToken(null);
+    setUsername(null); // clear the username when the user logs out
+
+  };
+
   return (
     <ApolloProvider client={client}>
-      <h2>My Book List</h2>
-      <BooksList />
+      <h2>My Book List App</h2>
+
+
+      {token ? (
+        <>
+        <button onClick={handleLogout}>Logout</button>
+        <p>Hello, {username}! Good to see you.</p> {/* display the username */}
+
+
+        <section className='main'>
+          <BooksList />
+          </section>
+
+        </>
+      ) : (
+        <LoginForm onLogin={handleLogin} />
+        
+      )}
+
+      
     </ApolloProvider>
   );
 }
---
+++ osa8/part-8c/books-frontend/src/components/LoginForm.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { useMutation, gql } from '@apollo/client';
+
+
+// LOGIN: username: 'ekauser', password: 'secret'
+const LOGIN_MUTATION = gql`
+mutation Login($username: String!, $password: String!) {
+  login(username: $username, password: $password) {
+    value
+  }
+}
+`;
+
+function LoginForm({ onLogin }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [login, { error }] = useMutation(LOGIN_MUTATION, {
+    onCompleted: (data) => {
+      const token = data.login.value;
+      localStorage.setItem('user-token', token);
+      onLogin(token, username);
+    },
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    login({ variables: { username, password } });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='loginform'>
+      <input value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+      <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" type="password" />
+      <button type="submit">Login</button>
+      {error && <p>Error :(</p>}
+    </form>
+  );
+}
+
+export default LoginForm;
