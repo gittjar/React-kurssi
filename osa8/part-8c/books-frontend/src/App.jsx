62b5143ad2412322e666df596b78381e8e675884
gittjar <jarno.keranen@gmail.com>
2023-12-28 19:32:48 +0200

    8.20 userin lempigenre

--- osa8/part-8c/books-frontend/src/App.jsx
+++ osa8/part-8c/books-frontend/src/App.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import { useState } from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
-import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom'; // Removed Switch
+import React, { useState, useEffect } from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, useLazyQuery } from '@apollo/client';
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import './styles.css';
 import BooksList from './components/BooksList';
 import LoginForm from './components/LoginForm';
@@ -14,54 +13,79 @@ const client = new ApolloClient({
   cache: new InMemoryCache()
 });
 
+const USER_QUERY = gql`
+query User($username: String!) {
+  user(username: $username) {
+    username
+    favoriteGenre
+  }
+}
+`;
+
 // Main App component
 function App() {
   const [token, setToken] = useState(localStorage.getItem('user-token'));
-  const [username, setUsername] = useState(null); // new state variable for the username
+  const [username, setUsername] = useState(null);
+  const [favoriteGenre, setFavoriteGenre] = useState(null);
+
+  const [getUser, { loading, data }] = useLazyQuery(USER_QUERY);
 
   const handleLogin = (newToken, username) => {
     setToken(newToken);
-    setUsername(username); // set the username
+    setUsername(username);
+    getUser({ variables: { username } }); // fetch the user's favorite genre
   };
 
   const handleLogout = () => {
     localStorage.removeItem('user-token');
     setToken(null);
-    setUsername(null); // clear the username when the user logs out
+    setUsername(null);
+    setFavoriteGenre(null);
   };
 
+  useEffect(() => {
+    if (data && data.user) {
+      setFavoriteGenre(data.user.favoriteGenre);
+    }
+  }, [data]);
+
+  return (
+    <Router>
+      <h2>My Book List App</h2>
+
+      {token ? (
+        <>
+          <nav>
+            <Link to="/show-books"><button>Show Books</button></Link>
+            <Link to="/add-book"><button>Add Book</button></Link>
+            <Link to="/update-author"><button>Update Author</button></Link>
+            <button onClick={handleLogout}>Logout</button>
+          </nav>
+
+          <p>Hello, {username}! Good to see you. Your favorite genre is {favoriteGenre}.</p>
+
+          <Routes>
+            <Route path="/add-book" element={<AddBookForm />} />
+            <Route path="/show-books" element={<BooksList />} />
+            <Route path="/update-author" element={<UpdateAuthorForm />} />
+          </Routes>
+        </>
+      ) : (
+        <Routes>
+          <Route path="/login" element={<LoginForm onLogin={handleLogin} />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      )}
+    </Router>
+  );
+}
+
+function AppWithProvider() {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <h2>My Book List App</h2>
-
-        {token ? (
-          <>
-            <nav>
-              <Link to="/show-books"><button>Show Books</button></Link>
-              <Link to="/add-book"><button>Add Book</button></Link>
-              <Link to="/update-author"><button>Update Author</button></Link>
-              <button onClick={handleLogout}>Logout</button>
-            </nav>
-
-            <p>Hello, {username}! Good to see you.</p>
-
-            <Routes>
-              <Route path="/add-book" element={<AddBookForm />} />
-              <Route path="/show-books" element={<BooksList />} />
-              <Route path="/update-author" element={<UpdateAuthorForm />} />
-            </Routes>
-          </>
-        ) : (
-          <Routes>
-            <Route path="/login" element={<LoginForm onLogin={handleLogin} />} />
-            <Route path="*" element={<Navigate to="/login" />} /> {/* Redirect to login page */}
-
-          </Routes>
-        )}
-      </Router>
+      <App />
     </ApolloProvider>
   );
 }
 
-export default App;
+export default AppWithProvider;
