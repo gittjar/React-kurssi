72a7e70ffc8aaea8aa1facb67fbec530115a915a
gittjar <jarno.keranen@gmail.com>
2024-01-02 20:15:27 +0200

    9.12 patientor backend step 5

--- osa9/osa-c/patientor-backend/src/index.ts
+++ osa9/osa-c/patientor-backend/src/index.ts
@@ -2,11 +2,13 @@
 
 import express from "express";
 import cors from "cors";
+import { v1 as uuid } from 'uuid';
 import diagnoses from "../data/diagnoses";
 import patients from "../data/patients";
 
 const app = express();
 app.use(cors());
+app.use(express.json()); // for parsing application/json
 
 app.get("/api/ping", (req, res) => {
   res.send("pong");
@@ -18,6 +20,20 @@ app.get("/api/diagnoses", (req, res) => {
 
 app.get("/api/patients", (req, res) => {
   res.send(patients);
+});
+
+app.post("/api/patients", (req, res) => {
+  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+  const newPatient = {
+    id: uuid(),
+    name,
+    dateOfBirth,
+    ssn,
+    gender,
+    occupation
+  };
+  patients.push(newPatient);
+  res.send(newPatient);
 });
 
 const PORT = process.env.PORT || 3003;
