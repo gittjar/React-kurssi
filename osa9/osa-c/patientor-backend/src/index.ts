e0aff37c3c683c3249cbfe7d718bb5e72d8940ce
gittjar <jarno.keranen@gmail.com>
2024-01-02 18:23:25 +0200

    9.10 patientor backend step 3

--- osa9/osa-c/patientor-backend/src/index.ts
+++ osa9/osa-c/patientor-backend/src/index.ts
@@ -1,11 +1,18 @@
+// src/index.ts
+
 import express from "express";
 import cors from "cors";
+import diagnoses from "../data/diagnoses";
 
 const app = express();
 app.use(cors());
 
 app.get("/api/ping", (req, res) => {
   res.send("pong");
+});
+
+app.get("/api/diagnoses", (req, res) => {
+  res.send(diagnoses);
 });
 
 const PORT = process.env.PORT || 3003;
