428d154e9690d3ffa24eb04b2de0b3a7b8216f11
gittjar <jarno.keranen@gmail.com>
2024-01-05 16:07:41 +0200

    9.18 notification

--- osa9/osa-d/flight-diary-frontend/src/App.tsx
+++ osa9/osa-d/flight-diary-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { DiaryEntry, Weather, Visibility } from './types';
 import DiaryEntryComponent from './DiaryEntry';
+import './styles.css';
 
 const App: React.FC = () => {
   const [diaryEntries, setDiaryEntries] = useState<DiaryEntry[]>([]);
@@ -11,6 +12,7 @@ const App: React.FC = () => {
     visibility: Visibility.Great,
     comment: '',
   });
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchDiaryEntries = async () => {
@@ -22,19 +24,29 @@ const App: React.FC = () => {
   }, []);
 
   const addEntry = async () => {
-    const response = await axios.post<DiaryEntry>('http://localhost:3001/api/diaries', newEntry);
-    setDiaryEntries([...diaryEntries, response.data]);
-    setNewEntry({ date: '', weather: Weather.Sunny, visibility: Visibility.Great, comment: '' });
+    try {
+      const response = await axios.post<DiaryEntry>('http://localhost:3001/api/diaries', newEntry);
+      setDiaryEntries([...diaryEntries, response.data]);
+      setNewEntry({ date: '', weather: Weather.Sunny, visibility: Visibility.Great, comment: '' });
+      setErrorMessage(null); // clear any previous error message
+    } catch (error) {
+      // If the error response has a message, use it, otherwise use a default message
+      setErrorMessage(error.response?.data?.message || 'An error occurred while adding the entry');
+    }
   };
 
   return (
     <div>
+      
+        {errorMessage && <article className='error-message'>Error: {errorMessage}</article>}
+      
+      <h1>My Flight Diary</h1>
       <div>
         <input
           value={newEntry.date}
           onChange={e => setNewEntry({ ...newEntry, date: e.target.value })}
           placeholder="Date"
-        />
+        /><br/>
         <select
           value={newEntry.weather}
           onChange={e => setNewEntry({ ...newEntry, weather: e.target.value as Weather })}
@@ -44,7 +56,7 @@ const App: React.FC = () => {
               {weather}
             </option>
           ))}
-        </select>
+        </select><br/>
         <select
           value={newEntry.visibility}
           onChange={e => setNewEntry({ ...newEntry, visibility: e.target.value as Visibility })}
@@ -54,12 +66,12 @@ const App: React.FC = () => {
               {visibility}
             </option>
           ))}
-        </select>
+        </select><br/>
         <textarea
           value={newEntry.comment}
           onChange={e => setNewEntry({ ...newEntry, comment: e.target.value })}
           placeholder="Comment"
-        />
+        /><br/>
         <button onClick={addEntry}>Add entry</button>
       </div>
       {diaryEntries.map(entry => (
