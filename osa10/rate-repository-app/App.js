409b743b7a0f6341f30b359b0f8b20aaaf9ed3d2
gittjar <jarno.keranen@gmail.com>
2024-01-09 20:30:36 +0200

    10.3 the reviewed repositories list

--- osa10/rate-repository-app/App.js
+++ osa10/rate-repository-app/App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import RepositoryList from './components/RepositoryList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! This is test!</Text>
+      <RepositoryList />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +17,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
